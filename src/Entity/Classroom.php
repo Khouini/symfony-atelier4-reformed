8c2fd2f4a2ca46a0ac7e6f1e5d4cabbea3683a3c
khouini <khouini.medyacine@gmail.com>
2023-03-26 12:26:35 +0100

    classroom only

--- src/Entity/Classroom.php
+++ src/Entity/Classroom.php
@@ -21,12 +21,10 @@ class Classroom
     // db
     private ?string $name = null;
 
-    #[ORM\OneToMany(mappedBy: 'classroom', targetEntity: Student::class, cascade:["persist", "remove"], orphanRemoval:true)]
-    private Collection $students;
+
 
     public function __construct()
     {
-        $this->students = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -42,36 +40,6 @@ class Classroom
     public function setName(string $name): self
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Student>
-     */
-    public function getStudents(): Collection
-    {
-        return $this->students;
-    }
-
-    public function addStudent(Student $student): self
-    {
-        if (!$this->students->contains($student)) {
-            $this->students->add($student);
-            $student->setClassroom($this);
-        }
-
-        return $this;
-    }
-
-    public function removeStudent(Student $student): self
-    {
-        if ($this->students->removeElement($student)) {
-            // set the owning side to null (unless already changed)
-            if ($student->getClassroom() === $this) {
-                $student->setClassroom(null);
-            }
-        }
 
         return $this;
     }
